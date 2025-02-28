c5e2286f2cd52affaa722adad57c43f6e3b76950
yumingkun <yumingkun@genfun.ai>
2025-02-28 19:15:19 +0800

    1

--- node.py
+++ node.py
@@ -21,6 +21,7 @@ from local_groundingdino.util.slconfig import SLConfig as local_groundingdino_SL
 from local_groundingdino.models import build_model as local_groundingdino_build_model
 import glob
 import folder_paths
+import matplotlib.pyplot as plt
 
 logger = logging.getLogger('comfyui_segment_anything')
 
@@ -215,6 +216,29 @@ def split_image_mask(image):
     return (image_rgb, mask)
 
 
+def show_mask(mask, ax, random_color=False):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30/255, 144/255, 255/255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+    
+def show_points(coords, labels, ax, marker_size=375):
+    pos_points = coords[labels==1]
+    neg_points = coords[labels==0]
+    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)   
+    
+def show_box(box, ax):
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))    
+
+
+
+
 def sam_segment(
     sam_model,
     image,
@@ -248,6 +272,7 @@ def sam_segment(
         mask_input=mask_input[None, :, :],
         multimask_output=False,
     )
+    
     
     return np.sum(bg)/np.max(bg) / np.sum(masks)
 
@@ -291,7 +316,7 @@ class GroundingDinoSAMSegment:
             item = image[i]
             pim = prediction_image[i]
 
-            item = item.cpu().to(torch.uint8).numpy()
+            item = item.cpu().to(torch.float16).numpy()
             pim = pim.cpu().to(torch.uint8).numpy()
 
             item = np.clip(255. * item , 0, 255).astype(np.uint8)
